8dabbfeeb4d23a81b2a2c1fed73581c94c7bc929
mfkasim91 <firman.kasim@gmail.com>
2020-04-15 23:45:30 +0100

    Runnable with inference

--- covidtracker/models.py
+++ covidtracker/models.py
@@ -9,17 +9,18 @@ class Model1(object):
     def __init__(self, a_bounds=(-2,3),
         b_bounds=(-1,0.5), logs_bounds=(-2,1),
         log_lscale_bounds=(0,3.0)):
+    # def __init__(self, a_bounds=(0,0.01),
+    #     b_bounds=(0.139,0.141), logs_bounds=(-8,-7),
+    #     log_lscale_bounds=(2.0,3.0)):
         """
         This model assumes:
-
-            y(t) ~ Poisson(mu(t))
-            mu(t) = exp(eps + a + integral(b(t)*dt))
-            eps ~ Laplace(0, sigma)
-            log(sigma) ~ Uniform(-2, 10)
+            log(y(t)) ~ Normal(mu(t), sigma)
+            mu(t) ~ a + integral(b(t)*dt)
+            log(sigma) ~ Uniform(*logs_bounds)
             b ~ GP(mean=0, kernel=RBF(sigma=bsigma, length_scale=lscale))
-            bsigma ~ Uniform(-1, 0.5)
-            log(lscale) ~ Uniform(0, 3.0)
-            a ~ Uniform(-2, 10)
+            bsigma ~ Uniform(*b_bounds)
+            log(lscale) ~ Uniform(*log_lscale_bounds)
+            a ~ Uniform(*a_bounds)
         """
 
         self.a_bounds = a_bounds
@@ -37,41 +38,31 @@ class Model1(object):
         a = pyro.sample("a", Uniform(*self.a_bounds))
         log_lscale = pyro.sample("log_lscale", Uniform(*self.log_lscale_bounds))
         lscale = torch.exp(log_lscale)
-        log_sigma_lb = nzero + self.logs_bounds[0]
-        log_sigma_ub = nzero + self.logs_bounds[1]
-        log_sigma = pyro.sample("log_sigma", Uniform(log_sigma_lb, log_sigma_ub))
+        log_sigma = pyro.sample("log_sigma", Uniform(*self.logs_bounds))
         tdist = t.unsqueeze(-1) - t # (n,n)
         b_sigma = pyro.sample("b_sigma", Uniform(*self.b_bounds))
         b_cov = b_sigma*b_sigma*torch.exp(-tdist*tdist / (2*lscale*lscale)) + torch.eye(n) * 1e-5
         b = pyro.sample("b", MultivariateNormal(nzero, b_cov))
-        eps = pyro.sample("eps", Laplace(nzero, torch.exp(log_sigma)))
 
         # calculate the rate
         int_bdt = torch.cumsum(b, dim=0) * dt
-        mu = torch.exp(a + int_bdt) # (n,)
-        plt.plot(b)
-        plt.plot(int_bdt)
-        plt.show()
-        plt.plot(eps)
-        plt.show()
+        mu = a + int_bdt # (n,)
 
         # simulate the observation
-        # NOTE: I'm using Normal here to approximate Poisson
-        mu_std = torch.clamp(torch.sqrt(mu), min=1.0)
-        ysim = pyro.sample("yt", Normal(mu, mu_std+torch.exp(log_sigma)))
+        logysim = pyro.sample("logyt", Normal(mu, torch.exp(log_sigma)))
 
-        return ysim
+        return logysim
 
-def conditioned_model(model, t, yt):
-    return poutine.condition(model.forward, data={"yt": yt})(t)
+def conditioned_model(model, t, logyt):
+    return poutine.condition(model.forward, data={"logyt": logyt})(t)
 
-def infer(args, model, t, yt):
+def infer(args, model, t, logyt):
     nuts_kernel = NUTS(conditioned_model, jit_compile=args.jit)
     mcmc = MCMC(nuts_kernel,
             num_samples=args.num_samples,
             warmup_steps=args.warmup_steps,
             num_chains=args.num_chains)
-    mcmc.run(model, t, yt)
+    mcmc.run(model, t, logyt)
     mcmc.summary(prob=0.5)
 
 def main():
@@ -86,20 +77,22 @@ def main():
     parser.add_argument('--jit', action='store_true', default=False)
     args = parser.parse_args()
 
-    t = torch.linspace(0, 30, 100)
+    t = torch.linspace(0, 10, 11)
     yt = torch.exp(0.14 * t)
     model = Model1()
-    infer(args, model, t, yt)
+    infer(args, model, t, torch.log(yt))
 
 if __name__ == "__main__":
+    # main()
     t = torch.linspace(0, 30, 100)
     yt = torch.exp(0.14 * t)
+    logyt = torch.log(yt)
     model = Model1()
 
-    alpha = 1.0
-    for i in range(1):
-        y = model.forward(t)
-        plt.plot(t, y, 'C0-', alpha=alpha)
-    plt.plot(t, yt, 'C1-')
+    alpha = 1.0 * 0.1
+    for i in range(100):
+        logy = model.forward(t)
+        plt.plot(t, logy, 'C0-', alpha=alpha)
+    plt.plot(t, logyt, 'C1-')
     # plt.gca().set_yscale("log")
     plt.show()
