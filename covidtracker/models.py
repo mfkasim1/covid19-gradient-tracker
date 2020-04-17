3ee059f77d10fec0694e2ca3eda4616e2b0473b4
mfkasim91 <firman.kasim@gmail.com>
2020-04-17 15:43:39 +0100

    Move plot_interval to plotter.py

--- covidtracker/models.py
+++ covidtracker/models.py
@@ -9,6 +9,7 @@ from pyro.infer import MCMC, NUTS
 import pyro.poutine as poutine
 import matplotlib.pyplot as plt
 from covidtracker.dataloader import DataLoader
+from covidtracker.plotter import plot_interval
 
 dtype=torch.float
 
@@ -102,19 +103,6 @@ def infer(args, model, t, yt):
     mcmc.run(model, t, yt)
     mcmc.summary(prob=0.95)
     return mcmc
-
-def plot_interval(t, ysamples, color="C0"): # numpy operation
-    ymedian = np.median(ysamples, axis=0)
-    yl1 = np.percentile(ysamples, 25., axis=0)
-    yu1 = np.percentile(ysamples, 75., axis=0)
-    yl2 = np.percentile(ysamples, 2.5, axis=0)
-    yu2 = np.percentile(ysamples, 97.5, axis=0)
-    yl3 = np.percentile(ysamples, 0.5, axis=0)
-    yu3 = np.percentile(ysamples, 99.5, axis=0)
-    plt.plot(t, ymedian, color=color, label="Median")
-    plt.fill_between(t, yl1, yu1, color=color, alpha=0.6, label="50% CI")
-    plt.fill_between(t, yl2, yu2, color=color, alpha=0.3, label="95% CI")
-    plt.fill_between(t, yl3, yu3, color=color, alpha=0.15, label="99% CI")
 
 class EmptyClass(object):
     def __init__(self):
