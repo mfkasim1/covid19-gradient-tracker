ffa0f6aa84f607c2f189f838a9cddcc1c502deaa
mfkasim91 <firman.kasim@gmail.com>
2020-04-19 22:04:11 +0100

    Refactoring with iterator generator

--- covidtracker/movement.py
+++ covidtracker/movement.py
@@ -34,7 +34,7 @@ class Information(object):
         self.fmovements_format = "data/fb/movements/movement_%Y-%m-%d_%H%M.csv"
         self.fsave_format = "data/fb/movements/movement_%Y-%m-%d_%H%M.pkl"
         self.tstart = datetime.datetime.strptime("2020-03-31 00:00", "%Y-%m-%d %H:%M")
-        self.tend = datetime.datetime.strptime("2020-05-01 00:00", "%Y-%m-%d %H:%M")
+        self.tend = datetime.datetime.today()
         self.tdelta = datetime.timedelta(hours=8)
         self.country = "ID"
 
@@ -46,58 +46,60 @@ class Information(object):
         fname = datetime.datetime.strftime(tnow, self.fmovements_format)
         return fname
 
+    def datetime_iter(self):
+        tnow = self.tstart
+        while tnow < self.tend:
+            yield tnow
+            tnow = tnow + self.tdelta
+
 def convert():
-    # specify the files
+    # the general helper class
     info = Information()
-    fmap = info.fmap
-    tstart = info.tstart
-    tend = info.tend
-    tdelta = info.tdelta
-    country = info.country
 
     # load the geometry of the map
-    map_geom = gp.read_file(fmap)
+    map_geom = gp.read_file(info.fmap)
 
-    tnow = tstart
     n_missing_points = 0
-    while tnow < tend:
-        # load the file
+    for tnow in info.datetime_iter():
         fname = info.get_fmovement(tnow)
         fsave = info.get_fsave(tnow)
         if not os.path.exists(fname) or os.path.exists(fsave):
-            tnow = tnow + tdelta
             continue
 
         movement_geom = pd.read_csv(fname)
         provinces_maps = {}
         print("Reading %s" % fname)
         for i in tqdm(range(movement_geom.shape[0])):
-            row = movement_geom.iloc[i]
-            if row.country != country:
+            rowdata = movement_geom.iloc[i]
+            if rowdata.country != info.country:
                 continue
-            start_point = Point(row.start_lon, row.start_lat)
-            end_point = Point(row.end_lon, row.end_lat)
+            start_point = Point(rowdata.start_lon, rowdata.start_lat)
+            end_point = Point(rowdata.end_lon, rowdata.end_lat)
 
             # get the starting and ending provinces
             start_province = find_province(map_geom, start_point)
+            if start_province is None:
+                n_missing_points += 1
+                continue
             end_province = find_province(map_geom, end_point)
-            if start_province is None or end_province is None:
+            if end_province is None:
                 n_missing_points += 1
                 continue
 
             key = "%s-%s" % (start_province, end_province)
-            add_key(provinces_maps, row.n_baseline, start_province, end_province, "n_baseline")
-            add_key(provinces_maps, row.n_crisis, start_province, end_province, "n_crisis")
+            add_key(provinces_maps, rowdata.n_baseline, start_province, end_province, "n_baseline")
+            add_key(provinces_maps, rowdata.n_crisis, start_province, end_province, "n_crisis")
 
         # save the provinces maps
         with open(fsave, "wb") as fb:
             pickle.dump(provinces_maps, fb)
 
-        # advancing the time
-        tnow = tnow + tdelta
-        # break
-
     print("Missing %d points" % n_missing_points)
+
+def main():
+    # convert the unconverted files
+    convert()
+
 
 if __name__ == "__main__":
     convert()
