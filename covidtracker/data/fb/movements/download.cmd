ac7f2f3c8561dadefb097f995a90310123b13e50
mfkasim91 <firman.kasim@gmail.com>
2020-04-22 10:43:22 +0100

    Update download

--- covidtracker/data/fb/movements/download.cmd
+++ covidtracker/data/fb/movements/download.cmd
@@ -1,6 +1,3 @@
-start https://www.facebook.com/geoinsights-portal/downloads/vector/?id=523934818561332^&ds=2020-04-18+0000
-start https://www.facebook.com/geoinsights-portal/downloads/vector/?id=523934818561332^&ds=2020-04-18+0800
-start https://www.facebook.com/geoinsights-portal/downloads/vector/?id=523934818561332^&ds=2020-04-18+1600
-start https://www.facebook.com/geoinsights-portal/downloads/vector/?id=523934818561332^&ds=2020-04-19+0000
-start https://www.facebook.com/geoinsights-portal/downloads/vector/?id=523934818561332^&ds=2020-04-19+0800
-start https://www.facebook.com/geoinsights-portal/downloads/vector/?id=523934818561332^&ds=2020-04-19+1600
+start https://www.facebook.com/geoinsights-portal/downloads/vector/?id=523934818561332^&ds=2020-04-20+0000
+start https://www.facebook.com/geoinsights-portal/downloads/vector/?id=523934818561332^&ds=2020-04-20+0800
+start https://www.facebook.com/geoinsights-portal/downloads/vector/?id=523934818561332^&ds=2020-04-20+1600
