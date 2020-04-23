1d631ea8e351674a2b265cf721b130d9fa3b1a51
mfkasim91 <firman.kasim@gmail.com>
2020-04-23 11:41:06 +0100

    Update download and jabar

--- covidtracker/data/fb/movements/download.cmd
+++ covidtracker/data/fb/movements/download.cmd
@@ -1,3 +1,3 @@
-start https://www.facebook.com/geoinsights-portal/downloads/vector/?id=523934818561332^&ds=2020-04-20+0000
-start https://www.facebook.com/geoinsights-portal/downloads/vector/?id=523934818561332^&ds=2020-04-20+0800
-start https://www.facebook.com/geoinsights-portal/downloads/vector/?id=523934818561332^&ds=2020-04-20+1600
+start https://www.facebook.com/geoinsights-portal/downloads/vector/?id=523934818561332^&ds=2020-04-21+0000
+start https://www.facebook.com/geoinsights-portal/downloads/vector/?id=523934818561332^&ds=2020-04-21+0800
+start https://www.facebook.com/geoinsights-portal/downloads/vector/?id=523934818561332^&ds=2020-04-21+1600
