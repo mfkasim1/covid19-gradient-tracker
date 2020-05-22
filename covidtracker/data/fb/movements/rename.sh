9369ad9a15abdfba7227ac9ab24fb93150ab6533
mfkasim91 <firman.kasim@gmail.com>
2020-05-22 13:14:15 +0100

    New format name

--- covidtracker/data/fb/movements/rename.sh
+++ covidtracker/data/fb/movements/rename.sh
@@ -1,2 +1,3 @@
-rename 's/Indonesia Coronavirus Disease Prevention Map Mar 31 2020 Id Movement between Administrative Regions/movement/' Indonesia\ Coronavirus\ Disease\ Prevention\ Map\ Mar\ 31\ 2020\ Id\ Movement\ between\ Administrative\ Regions_2020-0*
+rename 's/Indonesia Coronavirus Disease Prevention Map Mar 31 2020 Id Movement between Administrative Regions/movement/' Indonesia\ Coronavirus\ Disease\ Prevention\ Map\ Mar\ 31\ 2020\ Id\ Movement\ between\ Administrative\ Regions_*
+rename 's/__/_/' movement*
 rename 's/ /_/' movement_2020-0*
